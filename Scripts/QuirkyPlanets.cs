398e3fe5db815b1b5abde7a0d20cf8b40d8f45bd
talond <67006577+talon-d@users.noreply.github.com>
2023-04-24 21:39:18 -0500

    Add files via upload
    first commit

---
+++ Scripts/QuirkyPlanets.cs
@@ -0,0 +1,28 @@
+﻿using Stationeers.Addons;
+using Stationeers.Addons.API;
+using UnityEngine;
+
+namespace QuirkyPlanets.Scripts
+{
+    public class QuirkyPlanets : IPlugin
+    {
+        public void OnLoad()
+        {
+            Debug.Log("Example Mod: Hello, World!");
+
+            var monkey = BundleManager.GetAssetBundle("monkey").LoadAsset<Mesh>("monkey");
+
+            var basicModel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var basicModelTransform = basicModel.transform;
+            basicModel.GetComponent<MeshFilter>().sharedMesh = monkey;
+            basicModelTransform.position = Vector3.zero;
+            basicModelTransform.localScale = Vector3.one * 4.0f;
+            basicModelTransform.rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
+        }
+
+        public void OnUnload()
+        {
+            Debug.Log("Example Mod: Bye!");
+        }
+    }
+}
